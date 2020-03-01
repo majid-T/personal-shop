72a19f5c93baf1466c6272821a11940484690fde
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-03-01 02:45:26 -0500

    added routing to blog and made some templates

--- sample1/blog/views.py
+++ sample1/blog/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author':'Majid',
+        'title': 'myBlog',
+        'content': 'Content of my blogiing',
+        'date_posted': 'January 28, 2020'
+    },
+    {
+        'author':'Elahe',
+        'title': 'Eli Blog',
+        'content': 'Content of eli blogiing',
+        'date_posted': 'January 29, 2020'
+    }
+]
+
 # Create your views here.
+def blog(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/blog.html',context)
