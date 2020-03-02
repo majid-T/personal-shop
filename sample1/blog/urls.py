94e7bebf29e11fac394190db67f7d394ac4b5507
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-03-02 03:05:35 -0500

    Added models and DB

--- sample1/blog/urls.py
+++ sample1/blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.blog),
+    path('', views.blog, name='blog-home'),
+    path('about/',views.about, name='blog-about'),
 ]
--- sample1/blog/views.py
+++ sample1/blog/views.py
@@ -1,23 +1,13 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author':'Majid',
-        'title': 'myBlog',
-        'content': 'Content of my blogiing',
-        'date_posted': 'January 28, 2020'
-    },
-    {
-        'author':'Elahe',
-        'title': 'Eli Blog',
-        'content': 'Content of eli blogiing',
-        'date_posted': 'January 29, 2020'
-    }
-]
+from .models import Post
 
 # Create your views here.
 def blog(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all(),
+        'title': 'Blog home page'
     }
     return render(request, 'blog/blog.html',context)
+
+def about(request):
+    return render(request,'blog/about.html',{'title':'About'})
