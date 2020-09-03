1b963f1a8765ce898ff28c9f9609cb54578eab78
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-09-03 10:29:26 -0400

    Making models

--- theShop/api/models.py
+++ theShop/api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ShopItem(models.Model):
+    name = models.CharField(max_length=60)
+    desc = models.CharField(max_length=300)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
