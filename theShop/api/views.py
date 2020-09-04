6ddf6148ecc481194fdca91dc5171c7d00976689
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-09-04 12:46:49 -0400

    Added view backend

--- theShop/api/views.py
+++ theShop/api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets
+
+from .serializers import ShopItemSerializer
+from .models import ShopItem
+
+
+class ShopItemViewSet(viewsets.ModelViewSet):
+    queryset = ShopItem.objects.all().order_by('name')
+    serializer_class = ShopItemSerializer
+
 # Create your views here.
