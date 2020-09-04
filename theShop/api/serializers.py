5b97ca1756bec60c0e0ca10ee83764a627cb1b0c
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-09-04 12:54:49 -0400

    Added path and urls

--- theShop/api/serializers.py
+++ theShop/api/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import ShopItem
 
 
-class ShopItemSerializer (serializers.HyperLinkedModelSerializer):
+class ShopItemSerializer (serializers.HyperlinkedModelSerializer):
     class Meta:
         model = ShopItem
         fields = ('name', 'desc', 'price', 'quantity')
