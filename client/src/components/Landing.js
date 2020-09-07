1cefdc0c7e49bac7b6b5a2b7ee885beec39c6823
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-09-07 18:18:07 -0400

    Added spinner component and landing layout

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Spinner from "./Spinner";
 
 function Landing() {
-  return <div>Landing..</div>;
+  const url = "http://localhost:8000/api/shopItems/";
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const getItems = async () => {
+    console.log("called...");
+    let result = await axios.get(url);
+    let apiItems = result.data;
+    setItems(apiItems);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getItems();
+    console.log("items", items);
+  }, [loading]);
+  return (
+    <Container>
+      <Row>
+        <Col sm={4}>Panel</Col>
+        <Col sm={8}>{loading ? <Spinner /> : <div>items</div>}</Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Landing;
---
+++ client/src/components/Spinner.js
@@ -0,0 +1,11 @@
+import React from "react";
+import spinner from "../media/spinner.gif";
+export default () => (
+  <>
+    <img
+      src={spinner}
+      style={{ width: "200px", margin: "auto", display: "block" }}
+      alt="loading"
+    />
+  </>
+);
