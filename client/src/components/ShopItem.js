9e126e127b25dbe238f5c7e0180dcac461e6082b
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-11-23 21:32:08 -0500

    Added form in shopItem page

--- client/src/components/ShopItem.js
+++ client/src/components/ShopItem.js
@@ -1,17 +1,118 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Alert from "react-bootstrap/Alert";
+import Spinner from "react-bootstrap/Spinner";
 
 function ShopItem() {
     let { id } = useParams();
     const url = "http://localhost:8000/api/shopItems/";
-    const [item, setItem] = useState(null);
-    const [loading, setLoading] = useState(true);
+    // const [item, setItem] = useState(null);
+    const [errors, setErrors] = useState([]);
+    const [prompt, setPrompt] = useState(false);
+    const [propmtMsg, setPropmtMsg] = useState("");
+    const [promptClass, setPromptClass] = useState("");
+    const [btnMsg, setBtnMsg] = useState("add");
+    const [loading, setLoading] = useState(false);
+
+    //form values
+    const [formData, setFormData] = useState({
+        itemName: "",
+        itemDesc: "",
+        itemQ: "",
+        itemPrice: "",
+    });
+    const {
+        itemName,
+        itemDesc,
+        itemQ,
+        itemPrice,
+    } = formData;
+
+    const addShopItem = async () => {
+        const config = {
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+            },
+        };
+
+        const body = {
+            "name": itemName,
+            "desc": itemDesc,
+            "quantity": itemQ,
+            "price": itemPrice,
+        };
+        try {
+            setLoading(true);
+
+            const res = await axios.post(
+                `http://localhost:8000/api/shopItems/`,
+                body,
+                config
+            );
+
+            if (res.status == 201) {
+                setPrompt(true);
+                setErrors([])
+                setPropmtMsg(`New Item Added`);
+                setPromptClass('success');
+                setFormData({
+                    itemName: "",
+                    itemDesc: "",
+                    itemQ: "",
+                    itemPrice: "",
+                });
+                setLoading(false);
+                setTimeout(() => setPrompt(false), 5000);
+
+            } else {
+                setPrompt(true);
+                setPropmtMsg(`Something went wrong ... couldn't add Item`);
+                setPromptClass('danger');
+                console.log(res);
+                setLoading(false);
+            }
+        } catch (err) {
+            console.log("CATCH", err);
+            const responseErrors = []
+            if (err.response.data) {
+                for (const [key, value] of Object.entries(err.response.data)) {
+                    responseErrors.push(`${key}: ${value}`)
+                }
+                setErrors(responseErrors)
+            } else {
+                setErrors([`There was an error ${err}`])
+            }
+            setLoading(false);
+
+            setTimeout(() => setErrors([]), 5000);
+        }
+    };
+
+    const onChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        addShopItem();
+    };
 
     const getItem = async () => {
         let result = await axios.get(url + `${id}/`);
         let apiItem = result.data;
-        setItem(apiItem);
+        console.log(apiItem)
+        setFormData({
+            itemName: apiItem.name,
+            itemDesc: apiItem.desc,
+            itemQ: apiItem.quantity,
+            itemPrice: apiItem.price
+        })
+        // setItem(apiItem);
         setLoading(false);
     };
     useEffect(() => {
@@ -20,10 +121,53 @@ function ShopItem() {
 
 
     return (
-        <div>
-            <h1>This is shopItem page {id}</h1>
-            <p>{!loading && item.name}</p>
-            <p>{!loading && item.desc}</p>
+        <div className="editItemPanel">
+            {prompt ? <Alert variant={promptClass}>{propmtMsg}</Alert> : <></>}
+            {errors.length > 0 ? (errors.map(e => <Alert variant='danger'>{e}</Alert>)) : <></>}
+
+            {!loading && (
+                <Form onSubmit={(e) => onSubmit(e)}>
+                    <Form.Group controlId="itemName">
+                        <Form.Label>Item name</Form.Label>
+                        <Form.Control type="text" placeholder="Enter Item name"
+                            name="itemName"
+                            value={itemName}
+                            onChange={(e) => onChange(e)} />
+                    </Form.Group>
+
+                    <Form.Group controlId="itemDesc">
+                        <Form.Label>Description to your Item</Form.Label>
+                        <Form.Control type="text" placeholder="What is it?" name="itemDesc"
+                            value={itemDesc}
+                            onChange={(e) => onChange(e)} />
+                    </Form.Group>
+
+                    <Form.Row>
+                        <Col>
+                            <Form.Control type="number" placeholder="Quantity" name="itemQ"
+                                value={itemQ}
+                                onChange={(e) => onChange(e)} />
+                        </Col>
+                        <Col>
+                            <Form.Control type="number" step="0.01" placeholder="price" name="itemPrice"
+                                value={itemPrice}
+                                onChange={(e) => onChange(e)} />
+                        </Col>
+                    </Form.Row>
+                    <Button variant="primary" type="submit" className="my-2" >
+                        {loading && (
+                            <Spinner
+                                as="span"
+                                animation="border"
+                                size="sm"
+                                role="status"
+                                aria-hidden="true"
+                            />
+                        )}
+                        {btnMsg}
+                    </Button>
+                </Form>
+            )}
         </div>
     )
 }
