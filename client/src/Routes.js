17f04197639304f1b8da16758505b2a3100d0d17
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-11-25 20:49:17 -0500

    Navlink modification

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,18 +1,16 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import ShopItem from './components/ShopItem'
+import Landing from './components/Landing'
 // import NotFound from "../layout/NotFound";
 
 const Routes = () => {
   return (
     <section className="container">
       <Switch>
+        <Route exact path="/" component={Landing} />
         <Route exact path="/shop-item/:id" component={ShopItem} />
-        {/* <Route exact path="/register" component={Register} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/profiles" component={Profiles} />
-        <Route exact path="/profile/:id" component={Profile} />
-        <Route component={NotFound} /> */}
+        {/* <Route component={NotFound} />  */}
       </Switch>
     </section>
   );
