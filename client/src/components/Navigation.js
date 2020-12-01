cd8d8e2ebb87102314166be05bedb4aaf346972e
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-12-01 02:19:33 -0500

    Modified navigation for management

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -11,7 +11,7 @@ function Navigation() {
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
           <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
+          <Nav.Link href="/management">Management</Nav.Link>
           <NavDropdown title="Dropdown" id="basic-nav-dropdown">
             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
             <NavDropdown.Item href="#action/3.2">
