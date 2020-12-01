d3ef69471e56e2bcc4baeb839a8dea38919b2583
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-12-01 02:31:53 -0500

    Added simple home page and wired up in switch

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -3,10 +3,12 @@ import { Route, Switch } from "react-router-dom";
 import ShopItem from './components/ShopItem'
 import Management from './components/Management'
 import NotFound from "./components/NotFound";
+import Home from "./components/Home";
 
 const Routes = () => {
   return (
     <Switch>
+      <Route exact path="/" component={Home} />
       <Route exact path="/management" component={Management} />
       <Route exact path="/shop-item/:id" component={ShopItem} />
       <Route component={NotFound} />
